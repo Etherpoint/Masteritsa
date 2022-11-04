7b149e19f28bb4da3929df22daaf1e8983aa31f3
Danila Ryabov <danila02478@gmail.com>
2022-11-04 17:40:19 +0300

    prebuild for project

---
+++ src/main/java/ru/familyproject/ryabov/masteritsa/MasteritsaApplication.java
@@ -0,0 +1,13 @@
+package ru.familyproject.ryabov.masteritsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasteritsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasteritsaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/familyproject/ryabov/masteritsa/MasteritsaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.familyproject.ryabov.masteritsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasteritsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
