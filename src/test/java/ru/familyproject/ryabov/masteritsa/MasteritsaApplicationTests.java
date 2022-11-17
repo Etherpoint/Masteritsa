959e31c58c91271867e531487edc1f2081d101ba
Danila Ryabov <danila02478@gmail.com>
2022-11-17 15:54:15 +0300

    added @Deisales annotation in MasreritsaApplicationTests.class

--- src/test/java/ru/familyproject/ryabov/masteritsa/MasteritsaApplicationTests.java
+++ src/test/java/ru/familyproject/ryabov/masteritsa/MasteritsaApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//todo УБРАТЬ @DISABLED АННОТАЦИИ
 @SpringBootTest
 @Disabled
 class MasteritsaApplicationTests {
