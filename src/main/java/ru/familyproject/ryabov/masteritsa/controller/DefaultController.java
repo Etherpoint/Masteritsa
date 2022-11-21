c3e28cd3bb25b4d18ee471bc3b4f6c4dde0ee908
Danila Ryabov <danila02478@gmail.com>
2022-11-21 16:03:36 +0300

    added controller and deleted repeated code

---
+++ src/main/java/ru/familyproject/ryabov/masteritsa/controller/DefaultController.java
@@ -0,0 +1,19 @@
+package ru.familyproject.ryabov.masteritsa.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.familyproject.ryabov.masteritsa.repository.ProductTypeRepository;
+
+@Controller
+public class DefaultController {
+    @Autowired
+    ProductTypeRepository productTypeRepository;
+
+    @GetMapping("/")
+    public String index(Model model){
+
+        return "index";
+    }
+}
