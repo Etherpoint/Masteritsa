f7ae5850065821e37a4df2c9494a685e55831b50
Danila Ryabov <danila02478@gmail.com>
2022-11-23 15:13:29 +0300

    refactoring javadoc and endpoints

--- src/main/java/ru/familyproject/ryabov/masteritsa/controller/DefaultController.java
+++ src/main/java/ru/familyproject/ryabov/masteritsa/controller/DefaultController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.familyproject.ryabov.masteritsa.repository.ProductTypeRepository;
+import ru.familyproject.ryabov.masteritsa.utils.Endpoints;
 
 @Controller
 public class DefaultController {
@@ -14,9 +15,10 @@ public class DefaultController {
     /**GET метод к главной странице сайта по эндпоинту <b>"/"</b>
      *
      * @param model
+     * @see Endpoints#MAIN_PAGE
      * @return file <b>index.html</b>
      */
-    @GetMapping("/")
+    @GetMapping(Endpoints.MAIN_PAGE)
     public String index(Model model){
         return "index";
     }
@@ -24,9 +26,10 @@ public class DefaultController {
     /**GET метод к странице товара по эндпоинту <b>"/product"</b>
      *
      * @param model
+     * @see Endpoints#PRODUCT
      * @return file <b>product.html</b>
      */
-    @GetMapping("/product")
+    @GetMapping(Endpoints.PRODUCT)
     public String getProduct(Model model){
         return "product";
     }
