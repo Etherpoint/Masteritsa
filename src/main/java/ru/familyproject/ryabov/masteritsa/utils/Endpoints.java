63545959bc9884daf2492ae3ed2375c699bed6b8
Danila Ryabov <danila02478@gmail.com>
2022-11-23 15:07:15 +0300

    added util class Endpoints

---
+++ src/main/java/ru/familyproject/ryabov/masteritsa/utils/Endpoints.java
@@ -0,0 +1,6 @@
+package ru.familyproject.ryabov.masteritsa.utils;
+
+public class Endpoints {
+    public static final String MAIN_PAGE = "/";
+    public static final String PRODUCT = "/product";
+}
