6e1f0a14f4cde5810c7fa20918e6f8dcf7599dd3
Danila Ryabov <danila02478@gmail.com>
2022-11-27 21:03:48 +0300

    added new endpoints

--- src/main/java/ru/familyproject/ryabov/masteritsa/utils/Endpoints.java
+++ src/main/java/ru/familyproject/ryabov/masteritsa/utils/Endpoints.java
@@ -3,4 +3,8 @@ package ru.familyproject.ryabov.masteritsa.utils;
 public class Endpoints {
     public static final String MAIN_PAGE = "/";
     public static final String PRODUCT = "/product";
+
+    public static final String FILTER = "/filter";
+    public static final String FILTER_ALL = "/all";
+    public static final String FILTER_BY_ID = "/{id}";
 }
