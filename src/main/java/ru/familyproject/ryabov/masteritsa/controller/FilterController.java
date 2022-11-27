826fb230606177db1936e7fa96abc7d0bf550a7a
Danila Ryabov <danila02478@gmail.com>
2022-11-27 18:43:16 +0300

    added a new controller for sorting products

---
+++ src/main/java/ru/familyproject/ryabov/masteritsa/controller/FilterController.java
@@ -0,0 +1,46 @@
+package ru.familyproject.ryabov.masteritsa.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.familyproject.ryabov.masteritsa.entity.Product;
+import ru.familyproject.ryabov.masteritsa.entity.ProductType;
+import ru.familyproject.ryabov.masteritsa.service.ProductService;
+import ru.familyproject.ryabov.masteritsa.service.ProductTypeService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/filter")
+public class FilterController {
+    private final ProductService service;
+
+    public FilterController(@Autowired ProductService service, @Autowired ProductTypeService productTypeService) {
+        this.service = service;
+        this.productTypeService = productTypeService;
+    }
+
+    private final ProductTypeService productTypeService;
+
+
+    @GetMapping("/all")
+    public String getAllProducts(Model model) {
+        List<Product> products = service.getAll();
+        model.addAttribute("products", products);
+        List<ProductType> types = productTypeService.getAll();
+        model.addAttribute("types", types);
+        return "index";
+    }
+
+    @GetMapping("/{id}")
+    public String getAllProductsById(Model model,@PathVariable Long id){
+        List<Product> products = service.getAllById(id);
+        model.addAttribute("products", products);
+        List<ProductType> types = productTypeService.getAll();
+        model.addAttribute("types", types);
+        return "index";
+    }
+}
