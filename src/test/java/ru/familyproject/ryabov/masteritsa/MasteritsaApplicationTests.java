0fd7881867de72845d38e252663eed877189b1da
Danila Ryabov <danila02478@gmail.com>
2022-11-27 15:02:49 +0300

    optimised imports

--- src/test/java/ru/familyproject/ryabov/masteritsa/MasteritsaApplicationTests.java
+++ src/test/java/ru/familyproject/ryabov/masteritsa/MasteritsaApplicationTests.java
@@ -1,6 +1,5 @@
 package ru.familyproject.ryabov.masteritsa;
 
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
