a521156df71ddcc813e7ecb0276627cd2c9b8393
Danila Ryabov <danila02478@gmail.com>
2022-11-30 23:30:40 +0300

    added class for unit tests

---
+++ src/test/java/ru/familyproject/ryabov/masteritsa/service/ProductServiceTest.java
@@ -0,0 +1,16 @@
+package ru.familyproject.ryabov.masteritsa.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getAllById() {
+    }
+}
