e4b8961760fe4dea24e7c9a13fe3b3f0e7f65153
Danila Ryabov <danila02478@gmail.com>
2022-12-01 23:09:43 +0300

    added annotations for ProductTypeService

--- src/main/java/ru/familyproject/ryabov/masteritsa/service/ProductTypeService.java
+++ src/main/java/ru/familyproject/ryabov/masteritsa/service/ProductTypeService.java
@@ -3,19 +3,24 @@ package ru.familyproject.ryabov.masteritsa.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.familyproject.ryabov.masteritsa.entity.ProductType;
-import ru.familyproject.ryabov.masteritsa.repository.ProductTypeRepository;
 import ru.familyproject.ryabov.masteritsa.repository.ProductTypeRepositoryImpl;
 
 import java.util.List;
 
+/**Сервис для работы с сущностями <b>ProductType</b>*/
 @Service
-public class ProductTypeService {
-    private final ProductTypeRepository repository;
+public class ProductTypeService{
+    /**Приватное поле работы с сущностями <b>ProductType</b> в БД
+     * @see ProductTypeRepositoryImpl
+     * */
+    private final ProductTypeRepositoryImpl repository;
 
+    /**Конструктор для инициализации поля <b>repository</b>*/
     public ProductTypeService(@Autowired ProductTypeRepositoryImpl repository){
         this.repository = repository;
     }
 
+    /**Метод для получения всего списка типов продуктов*/
     public List<ProductType> getAll() {
         return repository.getAll();
     }
