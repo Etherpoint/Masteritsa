ff56f3cb5470980da7596486e4ef85cb40a721c6
Danila Ryabov <danila02478@gmail.com>
2022-12-01 22:53:04 +0300

    added annotations for ProductService

--- src/main/java/ru/familyproject/ryabov/masteritsa/service/ProductService.java
+++ src/main/java/ru/familyproject/ryabov/masteritsa/service/ProductService.java
@@ -7,17 +7,26 @@ import ru.familyproject.ryabov.masteritsa.repository.ProductRepositoryImpl;
 
 import java.util.List;
 
+/**Сервис для работы с сущностью Product*/
 @Service public class ProductService {
+    /**Приватный объект класса <b>ProductRepositoryImpl</b>
+     * @see ProductRepositoryImpl
+     */
     private final ProductRepositoryImpl repository;
 
+    /**Конструктор для инициализации переменной <b>repository</b>*/
     public ProductService(@Autowired ProductRepositoryImpl repository) {
         this.repository = repository;
     }
 
+    /**Метод для получения всего списка продуктов из БД*/
     public List<Product> getAll(){
         return repository.getAll();
     }
 
+    /**Метод для получения всех товаров, с product_type_id = id
+     * @see Product#getProductType()
+     */
     public List<Product> getAllById(Long id){
         return repository.getAllById(id);
     }
