bc730b2cc50b934afd43a7d9447ee18c3de6c5b5
Danila Ryabov <danila02478@gmail.com>
2022-12-02 18:19:27 +0300

    added unit-tests for ProductServiceTest

--- src/test/java/ru/familyproject/ryabov/masteritsa/service/ProductServiceTest.java
+++ src/test/java/ru/familyproject/ryabov/masteritsa/service/ProductServiceTest.java
@@ -1,16 +1,30 @@
 package ru.familyproject.ryabov.masteritsa.service;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import ru.familyproject.ryabov.masteritsa.repository.ProductRepositoryImpl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
 
+@SpringBootTest
 class ProductServiceTest {
+    @Autowired
+    private ProductService productService;
+    @MockBean
+    ProductRepositoryImpl repository;
 
     @Test
     void getAll() {
+        productService.getAll();
+        Mockito.verify(repository, Mockito.times(1)).getAll();
     }
 
     @Test
     void getAllById() {
+        Mockito.doReturn(new ArrayList<>()).when(repository).getAllById(1L);
+        Mockito.verify(repository, Mockito.times(1)).getAllById(1L);
     }
 }
