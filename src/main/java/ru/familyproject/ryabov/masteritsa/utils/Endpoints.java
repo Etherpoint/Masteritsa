44f7eac21cf09c111a9e8f77b3e2dfeaf6d69e7e
Danila Ryabov <danila02478@gmail.com>
2022-12-02 18:30:56 +0300

    rechanged endpoint

--- src/main/java/ru/familyproject/ryabov/masteritsa/utils/Endpoints.java
+++ src/main/java/ru/familyproject/ryabov/masteritsa/utils/Endpoints.java
@@ -4,7 +4,7 @@ public class Endpoints {
     public static final String MAIN_PAGE = "/";
     public static final String PRODUCT = "/product";
 
-    public static final String FILTER = "/filter";
+    public static final String FILTER = "/products";
     public static final String FILTER_ALL = "/all";
     public static final String FILTER_BY_ID = "/{id}";
 }
