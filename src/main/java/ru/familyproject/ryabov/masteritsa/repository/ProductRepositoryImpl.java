25ea708eb18f7e9f749030223a3ae3063ea27fc8
Danila Ryabov <danila02478@gmail.com>
2022-12-03 21:16:57 +0300

    changed configuration sessionFactory

--- src/main/java/ru/familyproject/ryabov/masteritsa/repository/ProductRepositoryImpl.java
+++ src/main/java/ru/familyproject/ryabov/masteritsa/repository/ProductRepositoryImpl.java
@@ -5,17 +5,14 @@ import org.hibernate.QueryException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
 import org.hibernate.query.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import ru.familyproject.ryabov.masteritsa.entity.Product;
 import ru.familyproject.ryabov.masteritsa.entity.ProductType;
-import ru.familyproject.ryabov.masteritsa.utils.CfgForHiber;
 
 import java.util.List;
-import java.util.Properties;
 
 @Repository
 public class ProductRepositoryImpl implements ProductRepository{
@@ -24,14 +21,8 @@ public class ProductRepositoryImpl implements ProductRepository{
 
     public ProductRepositoryImpl() {
         try{
-            Properties properties = new Properties();
-            properties.put(Environment.DRIVER, CfgForHiber.getDRIVER());
-            properties.put(Environment.URL, CfgForHiber.getURL());
-            properties.put(Environment.DIALECT, CfgForHiber.getDIALECT());
-            properties.put(Environment.USER, CfgForHiber.getUSER());
-            properties.put(Environment.PASS, CfgForHiber.getPASSWORD());
             sessionFactory= new Configuration()
-                    .addProperties(properties)
+                    .configure()
                     .addAnnotatedClass(Product.class)
                     .addAnnotatedClass(ProductType.class)
                     .buildSessionFactory();
--- src/main/java/ru/familyproject/ryabov/masteritsa/repository/ProductTypeRepositoryImpl.java
+++ src/main/java/ru/familyproject/ryabov/masteritsa/repository/ProductTypeRepositoryImpl.java
@@ -5,16 +5,13 @@ import org.hibernate.QueryException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
 import org.hibernate.query.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import ru.familyproject.ryabov.masteritsa.entity.ProductType;
-import ru.familyproject.ryabov.masteritsa.utils.CfgForHiber;
 
 import java.util.List;
-import java.util.Properties;
 
 @Repository
 public class ProductTypeRepositoryImpl implements ru.familyproject.ryabov.masteritsa.repository.ProductTypeRepository {
@@ -23,14 +20,8 @@ public class ProductTypeRepositoryImpl implements ru.familyproject.ryabov.master
 
     public ProductTypeRepositoryImpl() {
         try {
-            Properties properties = new Properties();
-            properties.put(Environment.DRIVER, CfgForHiber.getDRIVER());
-            properties.put(Environment.URL, CfgForHiber.getURL());
-            properties.put(Environment.DIALECT, CfgForHiber.getDIALECT());
-            properties.put(Environment.USER, CfgForHiber.getUSER());
-            properties.put(Environment.PASS, CfgForHiber.getPASSWORD());
             sessionFactory = new Configuration()
-                    .addProperties(properties)
+                    .configure()
                     .addAnnotatedClass(ProductType.class)
                     .buildSessionFactory();
             LOGGER.info("Configuration in ProductTypeRepositoryImpl was successful");
