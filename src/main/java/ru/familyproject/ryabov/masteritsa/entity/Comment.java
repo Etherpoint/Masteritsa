53c367d63475f4488a4f1581e395970d7cdf17c9
Danila Ryabov <danila02478@gmail.com>
2022-12-06 01:10:00 +0300

    added entity classes User and Comment

---
+++ src/main/java/ru/familyproject/ryabov/masteritsa/entity/Comment.java
@@ -0,0 +1,68 @@
+package ru.familyproject.ryabov.masteritsa.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity(name = "comment")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "date_of_create")
+    private Date dateOfCreate;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+//------------------------------------------- конец entity свойств-------------------------------------------------
+
+    public Comment() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDateOfCreate() {
+        return dateOfCreate;
+    }
+
+    public void setDateOfCreate(Date dateOfCreate) {
+        this.dateOfCreate = dateOfCreate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
---
+++ src/main/java/ru/familyproject/ryabov/masteritsa/entity/User.java
@@ -0,0 +1,45 @@
+package ru.familyproject.ryabov.masteritsa.entity;
+
+import javax.persistence.*;
+
+@Entity(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String name;
+
+    @Column(name = "image")
+    private String image;
+
+//------------------------------------------- конец entity свойств-------------------------------------------------
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
