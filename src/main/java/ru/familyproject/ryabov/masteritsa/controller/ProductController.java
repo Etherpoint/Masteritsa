0f65b5d1f9b1ea2c2e183415a4e32a6086bdd718
Danila Ryabov <danila02478@gmail.com>
2022-12-06 01:16:24 +0300

    added controller for Product.class

---
+++ src/main/java/ru/familyproject/ryabov/masteritsa/controller/ProductController.java
@@ -0,0 +1,40 @@
+package ru.familyproject.ryabov.masteritsa.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.familyproject.ryabov.masteritsa.entity.Comment;
+import ru.familyproject.ryabov.masteritsa.entity.Product;
+import ru.familyproject.ryabov.masteritsa.service.ProductService;
+import ru.familyproject.ryabov.masteritsa.utils.Endpoints;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(Endpoints.PRODUCT)
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(@Autowired ProductService productService) {
+        this.productService = productService;
+    }
+
+    /**
+     * GET метод к странице товара по эндпоинту <b>"/product/{id}"</b>
+     * @return file <b>product.html</b>
+     * @see Endpoints#PRODUCT
+     */
+    @GetMapping(Endpoints.FILTER_BY_ID)
+    public String getProduct(Model model, @PathVariable Long id) {
+        List<Comment> comments = productService.getAllCommentsById(id);
+        List<Product> types = productService.getAll();
+        Product product = productService.getById(id);
+        model.addAttribute("comments", comments);
+        model.addAttribute("types", types);
+        model.addAttribute("product", product);
+        return "product";
+    }
+}
