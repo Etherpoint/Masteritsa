caa315a422b3362821dd89e92abaaec4d00dc7d4
Danila Ryabov <danila02478@gmail.com>
2022-12-06 15:44:23 +0300

    fix bug

--- src/main/java/ru/familyproject/ryabov/masteritsa/controller/ProductController.java
+++ src/main/java/ru/familyproject/ryabov/masteritsa/controller/ProductController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.familyproject.ryabov.masteritsa.entity.Comment;
 import ru.familyproject.ryabov.masteritsa.entity.Product;
+import ru.familyproject.ryabov.masteritsa.entity.ProductType;
 import ru.familyproject.ryabov.masteritsa.service.ProductService;
+import ru.familyproject.ryabov.masteritsa.service.ProductTypeService;
 import ru.familyproject.ryabov.masteritsa.utils.Endpoints;
 
 import java.util.List;
@@ -17,9 +19,11 @@ import java.util.List;
 @RequestMapping(Endpoints.PRODUCT)
 public class ProductController {
     private final ProductService productService;
+    private final ProductTypeService productTypeService;
 
-    public ProductController(@Autowired ProductService productService) {
+    public ProductController(@Autowired ProductService productService, @Autowired ProductTypeService productTypeService) {
         this.productService = productService;
+        this.productTypeService = productTypeService;
     }
 
     /**
@@ -30,7 +34,7 @@ public class ProductController {
     @GetMapping(Endpoints.FILTER_BY_ID)
     public String getProduct(Model model, @PathVariable Long id) {
         List<Comment> comments = productService.getAllCommentsById(id);
-        List<Product> types = productService.getAll();
+        List<ProductType> types = productTypeService.getAll();
         Product product = productService.getById(id);
         model.addAttribute("comments", comments);
         model.addAttribute("types", types);
