dd31b77c5c5475d7380c42027b061346c6cfa4a9
Danila Ryabov <danila02478@gmail.com>
2022-12-09 20:53:25 +0300

    added new test cases for ProductService

--- src/test/java/ru/familyproject/ryabov/masteritsa/service/ProductServiceTest.java
+++ src/test/java/ru/familyproject/ryabov/masteritsa/service/ProductServiceTest.java
@@ -5,6 +5,9 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import ru.familyproject.ryabov.masteritsa.entity.Comment;
+import ru.familyproject.ryabov.masteritsa.entity.Product;
+import ru.familyproject.ryabov.masteritsa.repository.CommentRepositoryImpl;
 import ru.familyproject.ryabov.masteritsa.repository.ProductRepositoryImpl;
 
 import java.util.ArrayList;
@@ -14,18 +17,34 @@ class ProductServiceTest {
     @Autowired
     private ProductService productService;
     @MockBean
-    private ProductRepositoryImpl repository;
+    private ProductRepositoryImpl productRepository;
+    @MockBean
+    private CommentRepositoryImpl commentRepository;
 
     @Test
-    void getAll() {
+    void repositoryCallsOneTimeWhenCallsMethod_getAllInProductService() {
         productService.getAll();
-        Mockito.verify(repository, Mockito.times(1)).getAll();
+        Mockito.verify(productRepository, Mockito.times(1)).getAll();
     }
 
     @Test
-    void getAllById() {
-        repository.getAllById(1L);
-        Mockito.doReturn(new ArrayList<>()).when(repository).getAllById(1L);
-        Mockito.verify(repository, Mockito.times(1)).getAllById(1L);
+    void getListOfProductsWhenCallsMethod_getAllById() {
+        productRepository.getAllById(1L);
+        Mockito.doReturn(new ArrayList<Product>()).when(productRepository).getAllById(1L);
+        Mockito.verify(productRepository, Mockito.times(1)).getAllById(1L);
+    }
+
+    @Test
+    void getListOfCommentsWhenCallsMethod_getAllCommentsById(){
+        commentRepository.getAllCommentsById(1L);
+        Mockito.doReturn(new ArrayList<Comment>()).when(commentRepository).getAllCommentsById(1L);
+        Mockito.verify(commentRepository, Mockito.times(1)).getAllCommentsById(1L);
+    }
+
+    @Test
+    void getProductWhenCallsMethod_getById(){
+        productRepository.getById(1L);
+        Mockito.doReturn(new Product()).when(productRepository).getById(1L);
+        Mockito.verify(productRepository, Mockito.times(1)).getById(1L);
     }
 }
