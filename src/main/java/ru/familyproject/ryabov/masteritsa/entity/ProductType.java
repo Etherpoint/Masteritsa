6395b4f935701d48e64ff6899f8dc2fedbf3a137
Danila Ryabov <danila02478@gmail.com>
2022-12-09 17:07:32 +0300

    added allArgs constructor

--- src/main/java/ru/familyproject/ryabov/masteritsa/entity/ProductType.java
+++ src/main/java/ru/familyproject/ryabov/masteritsa/entity/ProductType.java
@@ -27,6 +27,11 @@ public class ProductType {
     public ProductType() {
     }
 
+    public ProductType(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
