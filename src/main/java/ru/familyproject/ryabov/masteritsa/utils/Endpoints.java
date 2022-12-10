561a9513ce50e5cbb0c55da7d830d3ebe719b42b
Danila Ryabov <danila02478@gmail.com>
2022-12-10 17:01:55 +0300

    refactoring code

--- src/main/java/ru/familyproject/ryabov/masteritsa/utils/Endpoints.java
+++ src/main/java/ru/familyproject/ryabov/masteritsa/utils/Endpoints.java
@@ -3,9 +3,9 @@ package ru.familyproject.ryabov.masteritsa.utils;
 /**Класс, содержащий все эндпоинты для веб-приложения*/
 public class Endpoints {
     public static final String MAIN_PAGE = "/";
-    public static final String PRODUCT = "/product/";
+    public static final String PRODUCT = "/product";
 
-    public static final String FILTER = "/products/";
+    public static final String FILTER = "/products";
     public static final String FILTER_ALL = "/all";
-    public static final String FIND_BY_ID = "{id}";
+    public static final String FIND_BY_ID = "/{id}";
 }
