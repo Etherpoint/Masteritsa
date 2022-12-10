3e1f6796945713ad6da7da280d64f02bc9b6c529
Danila Ryabov <danila02478@gmail.com>
2022-12-10 17:02:26 +0300

    added test cases for FilterController

---
+++ src/test/java/ru/familyproject/ryabov/masteritsa/controller/FilterControllerTest.java
@@ -0,0 +1,64 @@
+package ru.familyproject.ryabov.masteritsa.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.familyproject.ryabov.masteritsa.entity.Product;
+import ru.familyproject.ryabov.masteritsa.entity.ProductType;
+import ru.familyproject.ryabov.masteritsa.service.ProductService;
+import ru.familyproject.ryabov.masteritsa.service.ProductTypeService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.mockito.Mockito.*;
+
+@WebMvcTest
+class FilterControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    ProductService productService;
+    @MockBean
+    ProductTypeService productTypeService;
+
+    private List<ProductType> types;
+
+    private List<Product> products;
+
+    @BeforeEach
+    void init(){
+        this.types = new ArrayList<>();
+        types.add(new ProductType(1L, "Корона 1"));
+        types.add(new ProductType(2L, "Корона 2"));
+        types.add(new ProductType(3L, "Корона 3"));
+
+        this.products = new ArrayList<>();
+        products.add(new Product());
+        products.add(new Product());
+        products.add(new Product());
+    }
+
+
+    @Test
+    void doReturnProductsPageWhenCallsMethod_getAllProducts() throws Exception {
+        Mockito.when(productTypeService.getAll()).thenReturn(types);
+        this.mockMvc.perform(get("/products/all"));
+        verify(productTypeService, times(1)).getAll();
+    }
+
+    @Test
+    void doReturnProductsPageWhenCallsMethod_getAllProductsById() throws Exception {
+        Mockito.when(productTypeService.getAll()).thenReturn(types);
+        Mockito.when(productService.getAllById(1L)).thenReturn(products);
+        this.mockMvc.perform(get("/products/1"));
+        verify(productTypeService, times(1)).getAll();
+        verify(productService, times(1)).getAllById(1L);
+    }
+}
