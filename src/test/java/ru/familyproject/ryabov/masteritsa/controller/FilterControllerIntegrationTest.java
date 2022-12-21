8fc9dba5bc27fb3b65d4e2112622cdcff3f70f45
Danila Ryabov <danila02478@gmail.com>
2022-12-22 02:10:19 +0300

    added classes for integration tests

---
+++ src/test/java/ru/familyproject/ryabov/masteritsa/controller/FilterControllerIntegrationTest.java
@@ -0,0 +1,62 @@
+package ru.familyproject.ryabov.masteritsa.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class FilterControllerIntegrationTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void contentLoadingWhenCallsMethodGetAllProducts() throws Exception {
+        this.mockMvc.perform(get("/products/all"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Корона Славяночка")))
+                .andExpect(content().string(containsString("Корона Карнавал")))
+                .andExpect(content().string(containsString("image-1.jpg")))
+                .andExpect(content().string(containsString("image-2.jpg")));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "2,Корона Славяночка,image-1.jpg",
+            "9,Корона Карнавал,image-2.jpg"
+    })
+    void contentLoadingWhenCallsMethodGetAllProductsByIdEqualsOne(String id, String name, String imageSrc) throws Exception {
+        this.mockMvc.perform(get("/products/" + id))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString(name)))
+                .andExpect(content().string(containsString(imageSrc)));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "2,Корона Карнавал",
+            "9,Корона Cлавяночка",
+            "2,image-2.jpg",
+            "9,image-1.jpg"
+    })
+    void ThrowsAssertionErrorWhenCallsMethodGetAllProducts(String id, String unfoundInfo) {
+        Assertions.assertThrows(AssertionError.class,
+                () -> this.mockMvc.perform(get("/products/" + id))
+                        .andDo(print())
+                        .andExpect(status().isOk())
+                        .andExpect(content().string(containsString(unfoundInfo))));
+    }
+}
---
+++ src/test/java/ru/familyproject/ryabov/masteritsa/controller/ProductControllerIntegrationTest.java
@@ -0,0 +1,53 @@
+package ru.familyproject.ryabov.masteritsa.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProductControllerIntegrationTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @ParameterizedTest
+    @CsvSource({
+            "1,Корона Славяночка,image-1.jpg,Анна",
+            "6,Корона Карнавал,image-2.jpg,Елена",
+    })
+    void contentLoadingWhenCallsMethodGetProductById(String id, String name, String imageSrc, String userOfComment) throws Exception {
+        this.mockMvc.perform(get("/product/"+id))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString(name)))
+                .andExpect(content().string(containsString(imageSrc)))
+                .andExpect(content().string(containsString(userOfComment)));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1,Корона Карнавал",
+            "2,Корона Cлавяночка",
+            "1,image-2.jpg",
+            "6,image-1.jpg",
+            "1,Елена",
+            "6,Анна"
+    })
+    void ThrowsAssertionErrorWhenCallsMethodGetProductById(String id, String unfoundInfo) {
+        Assertions.assertThrows(AssertionError.class,
+                () -> this.mockMvc.perform(get("/products/" + id))
+                        .andDo(print())
+                        .andExpect(status().isOk())
+                        .andExpect(content().string(containsString(unfoundInfo))));
+    }
+}
