4826fbe8de852605376e1d10f242b5070de86830
Danila Ryabov <danila02478@gmail.com>
2023-01-04 23:31:32 +0300

    refactoring class Role for using in Spring Security

--- src/main/java/ru/familyproject/ryabov/masteritsa/entity/Role.java
+++ src/main/java/ru/familyproject/ryabov/masteritsa/entity/Role.java
@@ -1,14 +1,82 @@
 package ru.familyproject.ryabov.masteritsa.entity;
 
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.Set;
+
 /**
- * EN: Enum-class containing all possible user roles<br>
- * RU: Enum-класс, содержащий все возможные роли пользователя
+ * EN: User entity class with properties <b>id</b>, <b>name</b>, <b>users</b><br>
+ * RU: Entity-класс пользователя со свойствами <b>id</b>, <b>name</b>, <b>users</b>
+ *
+ * @see Entity
  *
  * @author Danila Ryabov
  *
  * @version 1.0
  */
-public enum Role {
-    USER,
-    ADMIN
+@Entity(name = "roles")
+public class Role implements GrantedAuthority {
+    /**
+     * EN: The id field in the database<br>
+     * RU: Поле id в базе данных
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * EN: Role name field<br>
+     * RU: Поле названия роли
+     */
+    @Column(name = "name")
+    private String name;
+
+    /**
+     * EN: Field for connection with the collection of users<br>
+     * RU: Поле для связи с коллекцией пользователей
+     */
+    @Column(name = "users")
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    //------------------------------------------- конец entity свойств-------------------------------------------------
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", users=" + users +
+                '}';
+    }
+
+    @Override
+    public String getAuthority() {
+        return getName();
+    }
 }
