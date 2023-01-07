8f71798210ccd8980e0a025f9592a385cd0b87f6
Danila Ryabov <danila02478@gmail.com>
2023-01-07 22:54:44 +0300

    uncommented for using custom login and registration page

--- src/main/java/ru/familyproject/ryabov/masteritsa/config/WebSecurityConfig.java
+++ src/main/java/ru/familyproject/ryabov/masteritsa/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -26,16 +27,18 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        //.logoutSuccessUrl("/")
         http.authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/product/**").authenticated()
                 .and()
-                .formLogin().permitAll();
+                //.formLogin().permitAll();
         //Custom login page
-                /*.formLogin((form) -> form
+                .formLogin((form) -> form
                         .loginPage("/login")
                         .permitAll()
-                );*/
+                )
+                .logout(LogoutConfigurer::permitAll);
         return http.build();
     }
 
