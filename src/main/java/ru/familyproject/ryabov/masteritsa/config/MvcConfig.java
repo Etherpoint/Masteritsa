d9cfd9bface605f74874bcdacac38b62f818e370
Danila Ryabov <danila02478@gmail.com>
2023-01-09 23:01:05 +0300

    added ResourceHandler

--- src/main/java/ru/familyproject/ryabov/masteritsa/config/MvcConfig.java
+++ src/main/java/ru/familyproject/ryabov/masteritsa/config/MvcConfig.java
@@ -1,7 +1,9 @@
 package ru.familyproject.ryabov.masteritsa.config;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,9 +18,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+    @Value("${upload.path}")
+    public String path;
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/registration").setViewName("registration");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/account-images/**")
+                .addResourceLocations("file://" + path.substring(3) + "/");
     }
 }
