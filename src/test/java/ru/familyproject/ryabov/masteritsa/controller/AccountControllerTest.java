9cc1332869329b0fc2d583732ee9d9ad078302e3
Danila Ryabov <danila02478@gmail.com>
2023-01-13 03:38:00 +0300

    refactoring test which checking exception

--- src/test/java/ru/familyproject/ryabov/masteritsa/controller/AccountControllerTest.java
+++ src/test/java/ru/familyproject/ryabov/masteritsa/controller/AccountControllerTest.java
@@ -1,14 +1,18 @@
 package ru.familyproject.ryabov.masteritsa.controller;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.ui.Model;
 import ru.familyproject.ryabov.masteritsa.entity.User;
+import ru.familyproject.ryabov.masteritsa.exceptions.AccountException;
 import ru.familyproject.ryabov.masteritsa.service.UserService;
 
 @SpringBootTest
@@ -18,21 +22,22 @@ class AccountControllerTest {
     @MockBean
     UserService userService;
     @MockBean
+    UserDetails userDetails;
+    @MockBean
     User user;
     @MockBean
     Model model;
 
-    @ParameterizedTest
-    @CsvSource({
-            "1",
-            "2",
-            "3"
-    })
-    void throwExceptionWhenUserIdNotEqualsParameterId(Long id) {
-        Mockito.when(user.getId()).thenReturn(0L);
+    @Test
+    @WithMockUser("Наталья")
+    void throwExceptionWhenUserIdNotEqualsParameterId() {
+        Mockito.when(userDetails.getUsername())
+                        .thenReturn("Наталья");
+        Mockito.when(userService.loadUserByUsername(Mockito.anyString()))
+                        .thenReturn(user);
         Assertions
-                .assertThrows(RuntimeException.class,
-                        () -> accountController.getAccount(user, id, model));
+                .assertThrows(AccountException.class,
+                        () -> accountController.getAccount(userDetails, 1L, model));
     }
 
     @ParameterizedTest
@@ -42,7 +47,9 @@ class AccountControllerTest {
             "3"
     })
     void shouldLoadUserByUsername_WhenUserIsNotNull_InMethodGetAllProducts(Long id) {
-        Mockito.when(user.getId()).thenReturn(id);
+        Mockito.when(
+                    user.getId())
+                .thenReturn(id);
         Mockito
                 .when(userService
                         .loadUserByUsername(user.getUsername()))
