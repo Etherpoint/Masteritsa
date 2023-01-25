88c9e8ec240911b0d8d9794b925f1be336355685
Danila Ryabov <danila02478@gmail.com>
2023-01-25 22:30:20 +0300

    changed package from utils to config and changed class configuration structure

---
+++ src/main/java/ru/familyproject/ryabov/masteritsa/config/MySessionFactory.java
@@ -0,0 +1,49 @@
+package ru.familyproject.ryabov.masteritsa.config;
+
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import ru.familyproject.ryabov.masteritsa.entity.Comment;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+/**
+ * EN: Singleton class for configuration <b>SessionFactory</b><br>
+ * RU: Класс-синглтон для конфигурации <b>SessionFactory</b>
+ * @see SessionFactory
+ */
+@Configuration
+@EnableTransactionManagement
+public class MySessionFactory {
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan(Comment.class.getPackageName());
+        sessionFactory.setHibernateProperties(hibernateProperties());
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/family_shop");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("postgres");
+        return dataSource;
+    }
+
+    private Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("spring.jpa.hibernate.ddl-auto", "none");
+        hibernateProperties.setProperty(
+                "hibernate.dialect", "org.hibernate.dialect.PostgreSQL82Dialect");
+        return hibernateProperties;
+    }
+}
