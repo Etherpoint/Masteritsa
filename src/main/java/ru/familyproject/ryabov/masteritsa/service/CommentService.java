4cf9d3885ff1e2e6789221f7cc1f79e65e50920a
Danila Ryabov <danila02478@gmail.com>
2023-02-03 23:03:20 +0300

    added method for saving comments and added javadoc

---
+++ src/main/java/ru/familyproject/ryabov/masteritsa/service/CommentService.java
@@ -0,0 +1,38 @@
+package ru.familyproject.ryabov.masteritsa.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.familyproject.ryabov.masteritsa.entity.Comment;
+import ru.familyproject.ryabov.masteritsa.repository.CommentRepository;
+import ru.familyproject.ryabov.masteritsa.repository.CommentRepositoryImpl;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Service
+public class CommentService {
+    /**
+     * EN: Service for working with entities <b>Comment</b> in the database<br>
+     * RU: Сервис для работы с сущностями <b>Comment</b> в БД
+     */
+    private final CommentRepository commentRepository;
+
+    /**
+     * EN: Service initialization constructor<br>
+     * RU: Конструктор для инициализации сервиса
+     */
+    public CommentService(@Autowired CommentRepositoryImpl commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    /**
+     * EN: Method for finding a comment in the database<br>
+     * RU: Метод для сохранения комментария в БД
+     */
+    public void saveComment(Comment comment) {
+        Date date = new Date();
+        SimpleDateFormat simpleDate = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        comment.setDateOfCreate(simpleDate.format(date));
+        commentRepository.saveComment(comment);
+    }
+}
