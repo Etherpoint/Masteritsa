8e2ecad910d125364bffefb8059de1705d472523
Danila Ryabov <danila02478@gmail.com>
2023-02-05 02:40:22 +0300

    added test cases for method saveComment

--- src/test/java/ru/familyproject/ryabov/masteritsa/repository/CommentRepositoryImplTest.java
+++ src/test/java/ru/familyproject/ryabov/masteritsa/repository/CommentRepositoryImplTest.java
@@ -3,9 +3,11 @@ package ru.familyproject.ryabov.masteritsa.repository;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -25,6 +27,10 @@ class CommentRepositoryImplTest {
     private Session session;
     @MockBean
     private Query<Comment> result;
+    @MockBean
+    private Comment comment;
+    @MockBean
+    private Transaction transaction;
 
     @Test
     void getSessionWhenSessionFactoryCalls_openSession_InMethodGetAllCommentsById() {
@@ -68,5 +74,46 @@ class CommentRepositoryImplTest {
                 .createQuery("SELECT c FROM comment c WHERE c.product.id= :id", Comment.class))
                 .thenReturn(result);
         commentRepository.getAllCommentsById(1L);
+    }
+
+    //-------------------------------------------Тесты для метода saveComment--------------------------------
+
+    @Test
+    void getSessionWhenSessionFactoryCalls_openSession_InMethod_saveComment() {
+        successfulMethod_saveComment();
+        verify(sessionFactory, times(1)).openSession();
+    }
+
+    @Test
+    void getTransactionWhenCalls_beginTransaction_InMethod_saveComment() {
+        successfulMethod_saveComment();
+        verify(session, times(1)).beginTransaction();
+    }
+
+    @Test
+    void methodSaveCallsOneTime_InMethod_saveComment() {
+        successfulMethod_saveComment();
+        verify(session, times(1))
+                .save(comment);
+    }
+
+    @Test
+    void methodCommitCallsOneTime_InMethod_saveComment() {
+        successfulMethod_saveComment();
+        verify(transaction, times(1)).commit();
+    }
+
+    @Test
+    void getErrorMessage_ErrorWhenOpenedSession_AndThrowHibernateException_WhenSessionFactoryOpenSessionInMethod_saveComment() {
+        when(sessionFactory.openSession()).thenThrow(HibernateException.class);
+        HibernateException exception = Assertions.assertThrows(HibernateException.class,
+                () -> commentRepository.saveComment(comment));
+        Assertions.assertEquals(exception.getMessage(), "Error when opened session on sessionFactory in method saveComment from CommentRepositoryImpl");
+    }
+
+    void successfulMethod_saveComment() {
+        Mockito.when(sessionFactory.openSession()).thenReturn(session);
+        Mockito.when(session.beginTransaction()).thenReturn(transaction);
+        commentRepository.saveComment(comment);
     }
 }
