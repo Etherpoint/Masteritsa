3d96253ca9091aaf3590d1c3bdb5a1fe967caef1
Danila Ryabov <danila02478@gmail.com>
2023-02-05 03:00:58 +0300

    added integration tests for functional saving comments

--- src/test/java/ru/familyproject/ryabov/masteritsa/controller/ProductControllerIntegrationTest.java
+++ src/test/java/ru/familyproject/ryabov/masteritsa/controller/ProductControllerIntegrationTest.java
@@ -7,14 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import ru.familyproject.ryabov.masteritsa.utils.Endpoints;
 
 import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -52,5 +54,22 @@ class ProductControllerIntegrationTest {
                         .andDo(print())
                         .andExpect(status().isOk())
                         .andExpect(content().string(containsString(unfoundInfo))));
+    }
+
+    @ParameterizedTest
+    @CsvSource(delimiterString = ", ",
+            value = {
+                    "1, Тестовый комментарий-1",
+                    "2, Тестовый комментарий-2",
+                    "3, Тестовый комментарий-3"
+            })
+    void saveCommentAndReloadPage(String id, String comment) throws Exception {
+        MockHttpServletRequestBuilder multipart = multipart(Endpoints.PRODUCT + "/" + id)
+                .param("description", comment)
+                .with(csrf());
+        this.mockMvc.perform(multipart.with(user("Наталья")))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl(Endpoints.PRODUCT + "/" + id));
     }
 }
