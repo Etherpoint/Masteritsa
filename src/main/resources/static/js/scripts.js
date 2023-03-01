502ebee357ec2be16834e9549ae61dba3b7ef0af
Danila Ryabov <danila02478@gmail.com>
2023-03-01 17:02:31 +0300

    added jquery button and js code for delete comment

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,18 +1,7 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.5 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
-
-function deleteComment(id) {
-    let url = "/comment/" + id
-    $.ajax({
-        url: url,
-        type: 'DELETE',
-        success: function (){
-            //добавить функцию для загрузки комментариев
-        }
+async function deleteComment(id) {
+    let url = "/comment/" + id;
+    await fetch(url, {
+        method: 'GET'
     })
+    window.location.reload()
 }
